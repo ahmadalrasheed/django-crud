24c8cfb0af0eafe0092ee2cf6011e82ef7b43772
ahmadalrasheed <alrasheedahmad97@gmail.com>
2021-11-16 20:17:15 +0200

    feat: added CRUD app

---
+++ snacks_crud_project/snacks/tests.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from django.urls import reverse
+
+# Create your tests here.
+
+class Tests(TestCase):
+    def test_snack_list_response(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code , 200)
+    def test_snack_create_response(self):
+        url = reverse('createsnack')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code , 200)
+    def test_snack_list_template(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response , 'snack_list_view.html')
+        self.assertTemplateUsed(response , 'base.html')
+    def test_snack_create_template(self):
+        url = reverse('createsnack')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response , 'snack_create_view.html')
+        self.assertTemplateUsed(response , 'base.html')
+    # def test_snack_list_template():
+    #     pass
---
+++ snacks_crud_project/snacks/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import (
+    SnackListView,
+    SnackDetailView,
+    SnackCreateView,
+    SnackUpdateView
+)
+
+
+urlpatterns = [
+    path('' , SnackListView.as_view() , name='home'),
+    path('<int:pk>/' , SnackDetailView.as_view() , name='snackdetails' ),
+    path('create/' , SnackCreateView.as_view() , name='createsnack'),
+    path('<int:pk>/update/' , SnackUpdateView.as_view()  , name='updatesnack')
+]
